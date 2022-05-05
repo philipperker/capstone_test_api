71678dd52b48bed2f136f58f7ebe49450d306b05
Philipp Erker <Philipp.erker@protonmail.com>
2022-05-05 14:14:57 +0200

    change index.js

--- index.js
+++ index.js
@@ -5,43 +5,83 @@ const port = process.env.PORT || 4444;
 //-------------example nft data----------------------
 const example_nft_data = [
   {
-    created_by: "BoredApeYachtClub",
-    contract_id: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
-    token_id: "8520",
-    token_url: "https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/8520",
-    image: "https://ipfs.io/ipfs/QmNfz5mmMs5yhk9SQubuBkuxi467uPLxwE43eQGBGi2JZt",
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "1",
+    token_url: "XXX-1",
+    image: "https://gateway.pinata.cloud/ipfs/QmcRKxkWrTpP9Z7G84Z1eiL4eTqkTVketdYS1oNPRxxq5n",
   },
 
   {
-    created_by: "BoredApeYachtClub",
-    contract_id: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
-    token_id: "3368",
-    token_url: "https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/3368",
-    image: "https://ipfs.io/ipfs/QmP37F4D6anmnVCzbgRUhawreyupsdbw59HvrcapNAvdzv",
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "2",
+    token_url: "XXX-2",
+    image: "https://gateway.pinata.cloud/ipfs/QmT3mHEzHfDjo9spq7SGfdUFYVBoZPPevPQVA5pm9kSeKW",
   },
 
   {
-    created_by: "BoredApeYachtClub",
-    contract_id: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
-    token_id: "4671",
-    token_url: "https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/4671",
-    image: "https://ipfs.io/ipfs/Qmci1pGaUmvb6StPxdGp1WqfK9QjPjdf43nCbY5LJ9y1MY",
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "3",
+    token_url: "XXX-3",
+    image: "https://gateway.pinata.cloud/ipfs/QmenQCrnqFoM7s7sG9tJvHNVUJoHDDqie48mk7TBJfjvcs",
   },
 
   {
-    created_by: "BoredApeYachtClub",
-    contract_id: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
-    token_id: "570",
-    token_url: "https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/570",
-    image: "https://ipfs.io/ipfs/QmPCKggCTEnpA8jTQBroPaCbV9rsMQ4asZ7keYJjjJDoHr",
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "4",
+    token_url: "XXX-4",
+    image: "https://gateway.pinata.cloud/ipfs/QmVEXHS7MknXCq63kKKdNHKtXnbx48BEnb5ayeNtLsb6gN",
   },
 
   {
-    created_by: "BoredApeYachtClub",
-    contract_id: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D",
-    token_id: "150",
-    token_url: "https://ipfs.io/ipfs/QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/150",
-    image: "https://ipfs.io/ipfs/QmR9uR6ufPnTrz2eFVrhgESEaU47Y7F4UE5pYorHNYR9XF",
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "5",
+    token_url: "XXX-5",
+    image: "https://gateway.pinata.cloud/ipfs/QmdNRUd5i7qG2QsMfswnSP39WwMJdTXXsvxv1pzNVCtQwj",
+  },
+
+  {
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "6",
+    token_url: "XXX-6",
+    image: "https://gateway.pinata.cloud/ipfs/QmQ4SAR1DNQBZauAAN5QbRv1AumjmGsCGEBqwMR6zUy44C",
+  },
+
+  {
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "7",
+    token_url: "XXX-7",
+    image: "https://gateway.pinata.cloud/ipfs/Qmcnt2RbabRQJkk381KG21mSZB5jeZ9NchbYPbKcu8wfLf",
+  },
+
+  {
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "8",
+    token_url: "XXX-8",
+    image: "https://gateway.pinata.cloud/ipfs/QmZ2TMiStcFcqc7qq7mkzstRvK2sPSV9pu1LUbMMHcJp4F",
+  },
+
+  {
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "9",
+    token_url: "XXX-9",
+    image: "https://gateway.pinata.cloud/ipfs/QmVsdFYTUsTmTBZfc5QBzYf5wxZHax2hCjCL1KhdGEG5Cr",
+  },
+
+  {
+    created_by: "ppilihp",
+    contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
+    token_id: "10",
+    token_url: "XXX-10",
+    image: "https://gateway.pinata.cloud/ipfs/QmXiokLC19Traorfc4AniXy1ikDVpPpXb3sUfLM5LDRxBb",
   },
 ];
 
