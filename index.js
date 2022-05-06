80cda0271b49feaffd460f272f9a51f35bf510c4
Philipp Erker <Philipp.erker@protonmail.com>
2022-05-06 11:15:10 +0200

    change protocl ipfs

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const example_nft_data = [
     contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
     token_id: "1",
     token_url: "XXX-1",
-    image: "https://gateway.pinata.cloud/ipfs/QmcRKxkWrTpP9Z7G84Z1eiL4eTqkTVketdYS1oNPRxxq5n",
+    image: "https://ipfs.io/ipfs/QmcRKxkWrTpP9Z7G84Z1eiL4eTqkTVketdYS1oNPRxxq5n",
   },
 
   {
@@ -17,7 +17,7 @@ const example_nft_data = [
     contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
     token_id: "2",
     token_url: "XXX-2",
-    image: "https://gateway.pinata.cloud/ipfs/QmT3mHEzHfDjo9spq7SGfdUFYVBoZPPevPQVA5pm9kSeKW",
+    image: "https://ipfs.io/ipfs/QmT3mHEzHfDjo9spq7SGfdUFYVBoZPPevPQVA5pm9kSeKW",
   },
 
   {
@@ -25,7 +25,7 @@ const example_nft_data = [
     contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
     token_id: "3",
     token_url: "XXX-3",
-    image: "https://gateway.pinata.cloud/ipfs/QmenQCrnqFoM7s7sG9tJvHNVUJoHDDqie48mk7TBJfjvcs",
+    image: "https://ipfs.io/ipfs/QmenQCrnqFoM7s7sG9tJvHNVUJoHDDqie48mk7TBJfjvcs",
   },
 
   {
@@ -33,7 +33,7 @@ const example_nft_data = [
     contract_id: "XXX0x6ED09b7178e08203b7320784292d0F46A79D2Ff0",
     token_id: "4",
     token_url: "XXX-4",
-    image: "https://gateway.pinata.cloud/ipfs/QmVEXHS7MknXCq63kKKdNHKtXnbx48BEnb5ayeNtLsb6gN",
+    image: "https://ipfs.io/ipfs/QmVEXHS7MknXCq63kKKdNHKtXnbx48BEnb5ayeNtLsb6gN",
   },
 ];
 
